e30c00e5a6765305ccf675f9404078cd83a68039
Hirushi Rathnayake <95017027+HirushiNisansala@users.noreply.github.com>
2023-02-23 10:20:46 +0530

    This is side bar

---
+++ sidebar/src/App.js
@@ -0,0 +1,21 @@
+import './App.css';
+import Toolbar from './Component/Toolbar';
+import Sidebar from './Component/Sidebar';
+import Backdrop from './Component/Backdrop';
+
+
+
+function App() {
+  return (
+   <div className=' App'>
+    <Toolbar />
+    <Sidebar />
+    <Backdrop />
+    
+    
+    </div>
+
+  )
+}
+
+export default App;
---
+++ sidebar/src/Component/Backdrop.js
@@ -0,0 +1,10 @@
+import React from 'react'
+import '../App.css';
+
+function Backdrop() {
+  return (
+    <div className='backdrop'></div>
+  )
+}
+
+export default Backdrop
